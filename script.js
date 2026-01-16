36d9a4881dcae4ed7b1a6fda1759c5fc11e5cffe
clintonthestrategist <clintonakabike100@gmail.com>
2026-01-16 15:56:02 +0100

    Final deliverable

--- script.js
+++ script.js
@@ -9,16 +9,20 @@ const mainParagraph = document.getElementById('paragraphlast');
 button.addEventListener('click', () => {
     const newValue = input.value;
     const newSelect = select.value;
+
+    const selectedOption = select.options[select.selectedIndex];
+    const newColor = selectedOption.getAttribute('data-color');
     
     if (newValue.trim() !== "") {
         mainSpan.textContent =  ` ${newValue}`;
         packageSpan.textContent = ` ${newSelect}`;
-
         mainParagraph.style.display = 'block';
-
         input.value = "";
     }
     else {
         alert("Please enter your name");
+    }
+    if (newSelect !== "" && newColor) {
+        document.body.style.backgroundColor = newColor
     }
 });
